eb79de74b8f32fef673cbdbea1ed6fc78f2b704d
amanda <zhujianwang001@gmail.com>
2021-09-24 17:14:19 +0800

    添加基础教程说明

--- src/P8_Tensorboard.py
+++ src/P8_Tensorboard.py
@@ -1,15 +1,18 @@
 from torch.utils.tensorboard import SummaryWriter
 import numpy as np
 from PIL import Image
+import cv2
 
 writer = SummaryWriter("logs")
-image_path = "data/train/ants_image/6240329_72c01e663e.jpg"
+image_path = "../imgs/dog.png"
 img_PIL = Image.open(image_path)
+img = cv2.imread(image_path)
 img_array = np.array(img_PIL)
 print(type(img_array))
 print(img_array.shape)
 
-writer.add_image("train", img_array, 1, dataformats='HWC')
+writer.add_image("pil", img_array, 1, dataformats='HWC')
+writer.add_image(tag="cv2", img_tensor=cv2.cvtColor(img, cv2.COLOR_BGR2RGB), global_step=0, dataformats='HWC')
 # y = 2x
 for i in range(100):
     writer.add_scalar("y=2x", 3*i, i)
---
+++ src/pytorch基础教程.md
@@ -0,0 +1,11 @@
+# pytorch基础教程
+
+
+
+
+
+
+
+
+## 参考链接
+* 1 [torch.cat](https://pytorch.org/docs/stable/generated/torch.cat.html)
